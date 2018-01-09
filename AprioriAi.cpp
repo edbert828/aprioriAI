ca346735ed092ee41065ece42792bcbd6aec31e4
Unknown <edbertextreme@gmail.com>
2018-01-10 00:35:34 +0700

    transform char to string

--- AprioriAi.cpp
+++ AprioriAi.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
 #include <fstream>
+#include<string>
 using namespace std;
 
 int main(){
+	string sttr[100];
+	 int i=0;
 	ifstream in("consolelist.txt");
-	cout<<"aaa";
 	if(!in){
 		cout<<"cannot find data";
 	}
 	else{
-		cout<<"aaa";
+	char str[255];
+	
+	while(in){
+		in.getline(str,255);
+		if(in){
+		 sttr[i]=str;
+		cout<<sttr[i]<<endl;
+		}
+		i++;
+	}
 	}
 }
