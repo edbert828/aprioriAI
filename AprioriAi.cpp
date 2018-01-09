877971aaa3e48798a94d67208eb5e5b91e3c262a
Unknown <edbertextreme@gmail.com>
2018-01-10 00:54:19 +0700

    making the list of item using linklist

--- AprioriAi.cpp
+++ AprioriAi.cpp
@@ -3,23 +3,38 @@
 #include<string>
 using namespace std;
 
-int main(){
-	string sttr[100];
-	 int i=0;
-	ifstream in("consolelist.txt");
-	if(!in){
-		cout<<"cannot find data";
+ifstream in("consolelist.txt");
+
+struct Cl{
+	int id;
+	string Consolename;
+	Cl *next;
+	Cl *prev;
+}*list,*tail;
+
+bool checktextdata(){
+		if(!in){
+		return false;
 	}
-	else{
+}
+void inputdata(){
+		
 	char str[255];
-	
 	while(in){
 		in.getline(str,255);
 		if(in){
-		 sttr[i]=str;
-		cout<<sttr[i]<<endl;
+		 
 		}
-		i++;
+		
 	}
+}
+int main(){
+	string sttr[100];
+	 int i=0;
+	 if(checktextdata==false)
+	cout<<"cannot find data";
+
+	else{
+		inputdata();
 	}
 }
