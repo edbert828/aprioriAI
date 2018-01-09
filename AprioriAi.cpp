74f5c9129d473c464fb84b725926fb57bb2fb9c1
Unknown <edbertextreme@gmail.com>
2018-01-10 00:06:56 +0700

    test on taking data from text file

--- AprioriAi.cpp
+++ AprioriAi.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
-
+#include <fstream>
 using namespace std;
 
 int main(){
-	
+	ifstream in("consolelist.txt");
+	cout<<"aaa";
+	if(!in){
+		cout<<"cannot find data";
+	}
+	else{
+		cout<<"aaa";
+	}
 }
