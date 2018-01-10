b2e1fc0bda0f78ff871734dd3dc24d25d5fb1b61
Unknown <edbertextreme@gmail.com>
2018-01-10 20:11:59 +0700

    Input data to linklist

--- AprioriAi.cpp
+++ AprioriAi.cpp
@@ -12,6 +12,27 @@ struct Cl{
 	Cl *prev;
 }*list,*tail;
 
+void inputtolist(string con,int id){
+	
+	Cl *temp=new Cl();
+	temp->id=id;
+	temp->Consolename=con;
+
+	if(list==NULL){
+	list=tail=temp;
+	}	
+	else{
+		;
+		tail->next=temp;
+		temp->prev=tail;
+		tail=temp;
+		
+	}
+	list->prev=NULL;
+	tail->next=NULL;
+
+		
+}
 bool checktextdata(){
 		if(!in){
 		return false;
@@ -20,12 +41,23 @@ bool checktextdata(){
 void inputdata(){
 		
 	char str[255];
+	string sttr;
+	int i=1;
 	while(in){
 		in.getline(str,255);
 		if(in){
-		 
+		 	sttr=str;
+		 	inputtolist(sttr,i);
 		}
+		i++;
+	}
+}
+void showdata(){
+	Cl *show=list;
+	while(show!=NULL){
 		
+		cout<<show->id<<"."<<show->Consolename<<endl;
+		show=show->next;
 	}
 }
 int main(){
@@ -36,5 +68,6 @@ int main(){
 
 	else{
 		inputdata();
+		showdata();
 	}
 }
